1a24d62b64eed271f84dd98311619792b0ff82b4
Jacob Kedar Krevat <jacobtie@gmail.com>
2025-09-28 20:00:41 -0400

    Adding dynamic input files

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/jacobtie/wikipedia-research-agent/internal/config"
 	"github.com/jacobtie/wikipedia-research-agent/internal/orchestrator"
@@ -15,8 +18,17 @@ func main() {
 }
 
 func run() error {
+	inputFile := flag.String("file", "default.txt", "provide the name of your input file in the input directory")
+	flag.Parse()
 	cfg := config.New()
-	results, err := orchestrator.New(cfg, "Write a research paper about Vlad the Impaler and how his policies impacted his kingdom").Run(context.Background())
+	if inputFile == nil || *inputFile == "" {
+		return fmt.Errorf("failed to parse file flag")
+	}
+	input, err := readInput(*inputFile)
+	if err != nil {
+		return err
+	}
+	results, err := orchestrator.New(cfg, input).Run(context.Background())
 	if err != nil {
 		return err
 	}
@@ -27,4 +39,12 @@ func run() error {
 		fmt.Println(result.Msg)
 	}
 	return nil
+}
+
+func readInput(inputFile string) (string, error) {
+	rawBytes, err := os.ReadFile(path.Join("input", inputFile))
+	if err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+	return string(rawBytes), nil
 }
